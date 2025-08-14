1796708a940239597ae14adefd0c659405168e6d
Diego Rosas <diegorosas19@gmail.com>
2025-08-13 21:21:01 -0300

    Criando a estreutura básica do projeto e criando e configurando o banco de dados.

--- src/main/java/br/com/alura/desafio_screensound/DesafioScreensoundApplication.java
+++ src/main/java/br/com/alura/desafio_screensound/DesafioScreensoundApplication.java
@@ -1,13 +1,21 @@
 package br.com.alura.desafio_screensound;
 
+import br.com.alura.desafio_screensound.principal.Principal;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DesafioScreensoundApplication {
+public class DesafioScreensoundApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DesafioScreensoundApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal();
+		principal.exibeMenu();
+		System.out.println("teste");
+	}
 }
