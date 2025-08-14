804866b226779e168001ebb2c9019c539522ed40
Diego Rosas <diegorosas19@gmail.com>
2025-08-13 21:02:34 -0300

    Upando o projeto

---
+++ src/main/java/br/com/alura/desafio_screensound/DesafioScreensoundApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.desafio_screensound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioScreensoundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioScreensoundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alura/desafio_screensound/DesafioScreensoundApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.desafio_screensound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioScreensoundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
